909279ba33462c88777e37fe398a3e799df99c5d
adeveloper-wq <dajesch@gmail.com>
2023-06-05 17:37:55 +0200

    Fix day history

--- history/history.go
+++ history/history.go
@@ -8,7 +8,6 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"sort"
 	"strconv"
 	"time"
 )
@@ -58,6 +57,8 @@ func Sync() {
 
 		prometheusExpressionListDayHistory := map[string]string{
 			// Number of all possible predictions.
+			// "OR vector(0)" is necessary, because otherwise if Prometheus has no data for the given time range,
+			// it will return no data instead of a zero value.
 			"prediction_service_subscription_count_total": "prediction_service_subscription_count_total OR vector(0)",
 			// Number of published predictions.
 			// For the day history we want a granularity of 30 minutes.
@@ -65,6 +66,8 @@ func Sync() {
 			// Therefore we get, how many predictions were published in the last 1800 seconds (30 minutes)
 			// and divide it by 15, to get how many predictions we published on average every 120 seconds.
 			// The division by 2 is necessary, because TODO (copied from Grafana).
+			// "OR vector(0)" is necessary, because otherwise if Prometheus has no data for the given time range,
+			// it will return no data instead of a zero value.
 			"average_prediction_service_predictions_count_total": "increase(prediction_service_predictions_count_total{}[1800s]) / 15 / 2 OR vector(0)",
 		}
 
@@ -79,6 +82,8 @@ func Sync() {
 		step := 30 * time.Minute
 		for key, expression := range prometheusExpressionListDayHistory {
 			// Fetch the data from Prometheus.
+			// Example response:
+			// {"status":"success","data":{"resultType":"matrix","result":[{"metric":{},"values":[[1685888801,"0"],[1685890601,"0"],[1685892401,"0"],[1685894201,"0"],[1685896001,"0"],[1685897801,"0"],[1685899601,"0"],[1685901401,"0"],[1685903201,"0"],[1685905001,"0"],[1685906801,"0"],[1685908601,"0"],[1685910401,"0"],[1685912201,"0"],[1685914001,"0"],[1685915801,"0"],[1685917601,"0"],[1685919401,"0"],[1685921201,"0"],[1685923001,"0"],[1685924801,"0"],[1685926601,"0"],[1685928401,"0"],[1685930201,"0"],[1685932001,"0"],[1685933801,"0"],[1685935601,"0"],[1685937401,"0"],[1685939201,"0"],[1685941001,"0"],[1685942801,"0"],[1685944601,"0"],[1685946401,"0"],[1685948201,"0"],[1685950001,"0"],[1685951801,"0"],[1685953601,"0"],[1685955401,"0"],[1685957201,"0"],[1685959001,"0"],[1685960801,"0"],[1685962601,"0"],[1685964401,"0"],[1685966201,"0"],[1685968001,"0"],[1685969801,"0"],[1685971601,"0"],[1685973401,"0"],[1685975201,"0"]]},{"metric":{"__name__":"prediction_service_subscription_count_total","instance":"prediction-service:8000","job":"staging-prediction-service"},"values":[[1685966201,"2"],[1685968001,"2"],[1685969801,"2"],[1685971601,"2"],[1685973401,"2"]]}]}}
 			prometheusResponse, err := http.Post(
 				baseUrl+"/api/v1/query_range",
 				"application/x-www-form-urlencoded",
@@ -127,24 +132,33 @@ func Sync() {
 			}
 
 			// Add the data to the history.
+			// We get two results. One where each timestamp is "0" and one where for each timestamp where we have data, the value is not "0".
+			// We want to merge these two results such that we always use the object with != "0" if available.
 			for _, result := range prometheusResponseParsed.Data.Result {
 				if dayHistory[key] == nil {
 					dayHistory[key] = result.Values
 				} else {
-					dayHistory[key] = append(dayHistory[key], result.Values...)
+					indicesToUpdate := make([]int, 0)
+					for _, value := range result.Values {
+						for i, existingValue := range dayHistory[key] {
+							if existingValue[0] == value[0] && value[1] != "0" {
+								indicesToUpdate = append(indicesToUpdate, i)
+							} else {
+								indicesToUpdate = append(indicesToUpdate, -i)
+							}
+						}
+					}
+					for i, index := range indicesToUpdate {
+						if index > 0 {
+							dayHistory[key][index] = result.Values[i]
+						}
+					}
 				}
 			}
 
 			// If we have less than 48 values, we have a gap in the data.
 			if len(dayHistory[key]) < 48 {
 				log.Warning.Println("Something went wrong while syncing history: We have less than 48 values for ", key)
-			}
-
-			// Sort the day history by time.
-			if len(prometheusExpressionListDayHistory) > 1 {
-				sort.Slice(dayHistory[key], func(i, j int) bool {
-					return dayHistory[key][i][0].(float64) < dayHistory[key][j][0].(float64)
-				})
 			}
 		}
 
