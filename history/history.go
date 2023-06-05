334353203fd02242c0b02de9b4958a48647d21ff
adeveloper-wq <dajesch@gmail.com>
2023-06-05 14:51:30 +0200

    Add day history

--- history/history.go
+++ history/history.go
@@ -1,6 +1,7 @@
 package history
 
 import (
+	"bytes"
 	"encoding/json"
 	"io/ioutil"
 	"monitor/log"
@@ -21,12 +22,24 @@ type Data struct {
 }
 
 type Result struct {
-	Metric map[string]string `json:"metric"`
-	Value  []interface{}     `json:"value"`
+	Metric Metric          `json:"metric"`
+	Values [][]interface{} `json:"values"`
+}
+
+type Metric struct {
+	Name     string `json:"__name__"`
+	Instance string `json:"instance"`
+	Job      string `json:"job"`
 }
 
 // Periodically sync the history from our Prometheus.
 func Sync() {
+	// Fetch the path under which we will save the json files.
+	staticPath := os.Getenv("STATIC_PATH")
+	if staticPath == "" {
+		panic("STATIC_PATH not set")
+	}
+
 	for {
 		log.Info.Println("Syncing history...")
 
@@ -36,19 +49,20 @@ func Sync() {
 			panic("PROMETHEUS_URL is not set")
 		}
 
-		prometheusExpressionListDayHistory := []string{
+		prometheusExpressionListDayHistory := map[string]string{
 			// Number of all possible predictions.
-			"prediction_service_subscription_count_total",
+			"prediction_service_subscription_count_total": "prediction_service_subscription_count_total",
 			// Number of published predictions.
 			// For the day history we want a granularity of 30 minutes.
 			// Each subscription is valid for 120 seconds.
 			// Therefore we get, how many predictions were published in the last 1800 seconds (30 minutes)
 			// and divide it by 15, to get how many predictions we published on average every 120 seconds.
 			// The division by 2 is necessary, because TODO (copied from Grafana).
-			"increase(prediction_service_predictions_count_total{}[1800s]) / 15 / 2",
+			"average_prediction_service_predictions_count_total": "increase(prediction_service_predictions_count_total{}[1800s]) / 15 / 2",
 		}
 
-		dayHistory := make(map[string][]Result)
+		dayHistory := make(map[string][][]interface{})
+		validDayHistory := true
 
 		currentTime := time.Now()
 
@@ -56,11 +70,16 @@ func Sync() {
 		while := currentTime.Add(-24 * time.Hour)
 		until := currentTime
 		step := 30 * time.Minute
-		for _, expression := range prometheusExpressionListDayHistory {
+		for key, expression := range prometheusExpressionListDayHistory {
 			// Fetch the data from Prometheus.
-			prometheusResponse, err := http.Get(baseUrl + "/api/v1/query_range?query=" + url.QueryEscape(expression) + "&start=" + while.Format(time.RFC3339) + "&end=" + until.Format(time.RFC3339) + "&step=" + step.String())
+			prometheusResponse, err := http.Post(
+				baseUrl+"/api/v1/query_range",
+				"application/x-www-form-urlencoded",
+				bytes.NewBufferString(
+					"query=("+url.QueryEscape(expression)+")&start="+while.Format(time.UnixDate)+"&end="+until.Format(time.UnixDate)+"&step="+step.String()))
 			if err != nil {
 				log.Warning.Println("Could not sync history:", err)
+				validDayHistory = false
 				break
 			}
 			defer prometheusResponse.Body.Close()
@@ -68,6 +87,7 @@ func Sync() {
 			body, err := ioutil.ReadAll(prometheusResponse.Body)
 			if err != nil {
 				log.Warning.Println("Could not sync history:", err)
+				validDayHistory = false
 				break
 			}
 
@@ -75,29 +95,37 @@ func Sync() {
 			var prometheusResponseParsed Response
 			if err := json.Unmarshal(body, &prometheusResponseParsed); err != nil {
 				log.Warning.Println("Could not sync history:", err)
+				validDayHistory = false
+				break
+			}
+
+			if prometheusResponseParsed.Status != "success" {
+				log.Warning.Println("Could not sync history:", prometheusResponseParsed.Status)
+				validDayHistory = false
+				break
+			}
+
+			if prometheusResponseParsed.Data.ResultType != "matrix" {
+				log.Warning.Println("Could not sync history: ResultType is not matrix")
+				validDayHistory = false
 				break
 			}
 
 			// Add the data to the history.
 			for _, result := range prometheusResponseParsed.Data.Result {
-				// Get the topic from the metric.
-				topic := result.Metric.Topic
-
-				// Get the values from the metric.
-				for _, value := range result.Values {
-					// Get the timestamp.
-					timestamp := value[0].(float64)
-
-					// Get the value.
-					value := value[1].(string)
-
-					// Add the value to the history.
-					dayHistory[topic] = append(dayHistory[topic], Result{
-						Metric: result.Metric,
-						Value:  []interface{}{timestamp, value},
-					})
-				}
+				dayHistory[key] = result.Values
 			}
+		}
+
+		if validDayHistory {
+			// Write the history update to the file.
+			statusJson, err := json.Marshal(dayHistory)
+			if err != nil {
+				log.Error.Println("Error marshalling history summary:", err)
+				validDayHistory = false
+				return
+			}
+			ioutil.WriteFile(staticPath+"day-history.json", statusJson, 0644)
 		}
 
 		// Sleep for 30 minutes.
